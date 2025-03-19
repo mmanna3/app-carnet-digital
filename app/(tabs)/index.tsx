dce13e38b65f848fb7589a9cdc001d0da585e37b
mmanna3 <mmmannna3@gmail.com>
2025-03-19 15:39:47 -0300

    muestro usuario guardado en el store

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,16 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useAuth } from '@/app/hooks/use-auth';
 
 export default function TabOneScreen() {
+  const { usuario } = useAuth();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.userText}>Usuario: {usuario || 'No hay usuario'}</Text>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -27,5 +31,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  userText: {
+    fontSize: 16,
+    marginTop: 20,
   },
 });
