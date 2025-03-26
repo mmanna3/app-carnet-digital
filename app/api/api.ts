27d1039c03b4b6ac989a0d9dbc4bc5d2b01550e9
mmanna3 <mmmannna3@gmail.com>
2025-03-26 01:12:33 -0300

    endpoint por ambiente

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,8 @@
 import { Client, LoginDTO, LoginResponseDTO, CambiarPasswordDTO } from './clients'
 import { HttpClientWrapper } from './http-client-wrapper'
+import { getEnvVars } from '../config/env'
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://192.168.0.193:5072'
+const { apiUrl } = getEnvVars()
 
 const httpClient = new HttpClientWrapper()
-export const api = new Client(API_BASE_URL, httpClient)
+export const api = new Client(apiUrl, httpClient)
---
+++ app/config/env.ts
@@ -0,0 +1,27 @@
+import Constants from 'expo-constants';
+
+type Environment = 'development' | 'production';
+
+const ENV = {
+  development: {
+    apiUrl: 'http://192.168.0.193:5072',
+  },
+  production: {
+    apiUrl: 'https://luefi.liga.com.ar',
+  },
+};
+
+const getEnvironment = (): Environment => {
+  // Si estamos en desarrollo (usando expo start)
+  if (__DEV__) {
+    return 'development';
+  }
+  
+  // Si estamos en producción (build de la app)
+  return 'production';
+};
+
+export const getEnvVars = () => {
+  const environment = getEnvironment();
+  return ENV[environment];
+}; 
