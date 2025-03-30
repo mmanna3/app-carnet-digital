67d3a11a4871751b53909973953384c75e703f0c
mmanna3 <mmmannna3@gmail.com>
2025-03-30 03:56:50 -0300

    actualiza contrato con api

--- app/api/clients.ts
+++ app/api/clients.ts
@@ -1226,6 +1226,47 @@ export class Client {
     /**
      * @return Success
      */
+    jugadorDELETE(id: number): Promise<number> {
+        let url_ = this.baseUrl + "/api/Jugador/{id}";
+        if (id === undefined || id === null)
+            throw new Error("The parameter 'id' must be defined.");
+        url_ = url_.replace("{id}", encodeURIComponent("" + id));
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "DELETE",
+            headers: {
+                "Accept": "text/plain"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processJugadorDELETE(_response);
+        });
+    }
+
+    protected processJugadorDELETE(response: Response): Promise<number> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+                result200 = resultData200 !== undefined ? resultData200 : <any>null;
+    
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<number>(null as any);
+    }
+
+    /**
+     * @return Success
+     */
     jugadorGET(id: number): Promise<JugadorDTO> {
         let url_ = this.baseUrl + "/api/Jugador/{id}";
         if (id === undefined || id === null)
