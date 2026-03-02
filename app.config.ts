da387c4f6d08b2452601230293d0f369d6b32a55
mmanna3 <mmmannna3@gmail.com>
2026-03-01 21:05:53 -0300

    husky y quita versión de app.config (Expo lo maneja solo)

--- app.config.ts
+++ app.config.ts
@@ -67,7 +67,6 @@ const ligasDisponibles = esMultiliga
 export default (): ExpoConfig => ({
   name: config.splashScreenName ?? config.appName,
   slug: config.expoSlug,
-  version: '4.0.0',
   orientation: 'portrait',
   icon: config.icon,
   scheme: config.scheme,
