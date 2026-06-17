69083140563f3022a710659ee2d2c792fb9325f7
mmanna3 <mmmannna3@gmail.com>
2026-06-17 15:12:22 -0300

    sube versión

--- app.config.ts
+++ app.config.ts
@@ -67,7 +67,7 @@ const ligasDisponibles = esMultiliga
 export default (): ExpoConfig => ({
   name: config.splashScreenName ?? config.appName,
   slug: config.expoSlug,
-  version: '5.1.0',
+  version: '6.0.0',
   orientation: 'portrait',
   icon: config.icon,
   scheme: config.scheme,
